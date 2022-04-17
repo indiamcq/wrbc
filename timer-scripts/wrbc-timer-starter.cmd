ecf7ffeff98f8e32a333cbdc55eb2ac5b8ee9f2d
indiamcq <india.mcq@gmail.com>
2022-04-17 23:42:10 +1000

    removed some duplication

--- timer-scripts/wrbc-timer-starter.cmd
+++ timer-scripts/wrbc-timer-starter.cmd
@@ -3,38 +3,42 @@ echo Waiting to start the countdown timer.
 echo Do not exit this program.
 echo.
 rem find the day of the week
-for /f %%i in ('wmic path win32_localtime get dayofweek') do set dow=%%i
-if "%dow%" == "7" (
-rem check to see if it is sunday
-echo It is Sunday.
-if "%date:~0,5%" == "25/12" (
-echo Christmas Day
-set thour= 8
-) else (
+set message=Sunday
 set thour= 9
+for /f %%i in ('wmic path win32_localtime get dayofweek') do (
+  if "%%i" == "0" (
+    rem check to see if it is sunday
+    if "%date:~0,5%" == "25/12" (call :starter "Christmas Day" & goto :eof) 
+    call :starter "%message%"
+    goto :eof
+  )
 )
-goto :check1
-) else (
-set thour= 8
 rem test for Easter Friday or Christmas acceptable dates until the end of 2032
-if "%date:~0,10%" == "15/04/2022" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "07/04/2023" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "29/03/2024" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "18/04/2025" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "03/04/2026" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "26/03/2027" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "14/04/2028" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "30/03/2029" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "19/04/2030" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "11/04/2031" (echo Easter Friday & goto :check1 )
-if "%date:~0,10%" == "26/03/2032" (echo Easter Friday & goto :check1 )
-if "%date:~0,5%" == "25/12" (echo Christmas Day & goto :check1 )
+if "%date:~0,10%" == "15/04/2022" call :starter "Easter Friday"
+if "%date:~0,10%" == "07/04/2023" call :starter "Easter Friday"
+if "%date:~0,10%" == "29/03/2024" call :starter "Easter Friday"
+if "%date:~0,10%" == "18/04/2025" call :starter "Easter Friday"
+if "%date:~0,10%" == "03/04/2026" call :starter "Easter Friday"
+if "%date:~0,10%" == "26/03/2027" call :starter "Easter Friday"
+if "%date:~0,10%" == "14/04/2028" call :starter "Easter Friday"
+if "%date:~0,10%" == "30/03/2029" call :starter "Easter Friday"
+if "%date:~0,10%" == "19/04/2030" call :starter "Easter Friday"
+if "%date:~0,10%" == "11/04/2031" call :starter "Easter Friday"
+if "%date:~0,10%" == "26/03/2032" call :starter "Easter Friday"
+if "%date:~0,5%" == "25/12" call :starter "Christmas Day"
 rem The next line is for testing only. So normally should start with a "rem".
 rem if "%date:~4,5%" == "%date:~4,5%" (echo test date found %date:~4,5% & goto :check1 )
 )
 echo No church service day/date found.
 echo This program will exit!
 timeout /t 5 /nobreak
+goto :eof
+
+:starter
+echo It is %~1
+if "%~1" == "Christmas Day" set thour= 8
+if "%~1" == "Easter Friday" set thour= 8
+goto :check1
 goto :eof
 
 :check1
